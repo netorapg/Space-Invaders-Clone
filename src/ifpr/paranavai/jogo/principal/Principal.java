38d79ded329612d64f3a13791a32b537f0774189
Argustor <renatoplatz11@outlook.com>
2023-05-23 21:41:28 -0300

    new update

--- src/ifpr/paranavai/jogo/principal/Principal.java
+++ src/ifpr/paranavai/jogo/principal/Principal.java
@@ -1,5 +1,18 @@
 package ifpr.paranavai.jogo.principal;
 
-public class Principal {
-    
+import javax.swing.JFrame;
+
+public class Principal extends JFrame{
+    public Principal(){
+    super.setTitle("Space Invaders Clone");
+    super.setSize(1024, 728);
+    super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    super.setLocationRelativeTo(null);
+    super.setResizable(false);
+    super.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Principal();
+    }
 }
