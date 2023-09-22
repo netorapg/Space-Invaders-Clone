e97f65875c5ef62dadb4348b2917076431c4ed67
netorapg <renatoplatz11@outlook.com>
2023-09-21 21:12:49 -0300

    new update

---
+++ src/main/java/ifpr/paranavai/jogo/principal/Principal.java
@@ -0,0 +1,21 @@
+package ifpr.paranavai.jogo.principal;
+import javax.swing.JFrame;
+
+import ifpr.paranavai.jogo.modelo.FaseUm;
+
+public class Principal extends JFrame{
+    public Principal(){
+    FaseUm fase = new FaseUm();
+    super.add(fase);
+    super.setTitle("Space Invaders Clone");
+    super.setSize(800, 640);
+    super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    super.setLocationRelativeTo(null);
+    super.setResizable(false);
+    super.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Principal();
+    }
+}
