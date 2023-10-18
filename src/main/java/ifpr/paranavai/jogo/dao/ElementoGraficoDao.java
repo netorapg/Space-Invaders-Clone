e2922c1d603a69d1c57fd5dd1f37b25af3916133
netorapg <renatoplatz11@outlook.com>
2023-10-18 20:56:08 -0300

    conectando o Elemento Grafico ao banco de dados

--- src/main/java/ifpr/paranavai/jogo/dao/ElementoGraficoDao.java
+++ src/main/java/ifpr/paranavai/jogo/dao/ElementoGraficoDao.java
@@ -1,5 +1,12 @@
 package ifpr.paranavai.jogo.dao;
 
-public class ElementoGraficoDao {
-    
+import java.util.List;
+import ifpr.paranavai.jogo.modelo.ElementoGrafico;
+
+public interface ElementoGraficoDao {
+    public List<ElementoGrafico> buscarTodos();
+    public ElementoGrafico buscarPorId(Integer id);
+    public void atualizar(ElementoGrafico elementoGrafico);
+    public void excluir(ElementoGrafico elementoGrafico);
+    public void inserir(ElementoGrafico elementoGrafico);
 }
